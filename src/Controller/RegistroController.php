9db35b1c9e2e01c5b0124ad670edaa890bd4cd50
AntonioGP <agordonparedes@safareyes.es>
2023-02-03 13:32:55 +0100

    Registro

--- src/Controller/RegistroController.php
+++ src/Controller/RegistroController.php
@@ -1,51 +1,32 @@
 <?php
 
-$conexion = mysqli_connect("localhost","root","12345", "prueba" );//correcto
-if (!$conexion){
-    echo 'Error al conectar a la base de datos';
-}
-else {
-    echo 'Conectado a la base de datos';
-}
+use Symfony\Component\Routing\Annotation\Route;
 
-include 'cn.php';
-//Recibir los datos y almacenarlos en variables
-$nombre = $_POST["nombre"];
-$apellidos = $_POST["apellidos"];
-$correo = $_POST["correo"];
-$usuario = $_POST["usuario"];
-$contrasenya = $_POST["contraseña"];
-$genero = $_POST["genero"];
-$fechaNacimiento = $_POST["fechaNacimiento"];
+class RegistroController extends AbstractController
+{
 
-
-
-$verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario = '$usuario'");//correcto
-
-
-if (mysqli_num_rows($verificar_usuario) > 0){
-    echo 'El usuario ya esta registrado';
-    exit;
-}else{
-
-    $insertar =mysqli_query($conexion, "INSERT INTO usuarios(nombre, apellidos, correo, usuario, contraseña, genero , fechaNacimiento) VALUES ('$nombre', '$apellidos', '$correo', '$usuario', '$contrasenya', '$genero' ,'$fechaNacimiento' ");
-
-
-    if (!$insertar){
-        echo 'Error al registrarse';
-
-    }else {
-        echo'Usuario registrado exitosamente';
+    #[Route('/registro', name: 'app_registro', methods: ["POST"])]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher): JsonResponse
+    {
+        $user = new Usuario();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+            return $this->redirectToRoute('app_homepage');
+        }
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
     }
-
-
-
-
-
 }
-
-
-
-
-//Cerrar conexion
-mysqli_close($conexion);
